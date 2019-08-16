53e8ef0c9a3b15e8f97503bf85bef08529071c06
kgu <35379529+karengu30@users.noreply.github.com>
2019-08-16 12:07:31 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -6,27 +6,23 @@ fluidPage(
   hr(),
   
   fluidRow(
-    column(3,
-    h4("Type of Graph"),
+    column(3, br(),
     selectInput(inputId = "graph",
                 label = "Type of graph:",
                 choices = c("Histogram",
                             "Histogram with Inhibition",
                             "Sigmoidal Fit",
                             "All graphs")),
-    br(),
-    h4("Code on:"),
-    tags$a("Github", 
-           href="http://www.econometricsbysimulation.com"),
-    br(),
     sliderInput("obs", "Observations: ",
                 min = 1, max = 1000, value = 600),
-    
+    h3("Code on:"),
+    tags$a("Github", 
+           href="https://github.com/karengu30/ShinyApp-Two-Horse-Race-Model"),
     br(),
     actionButton("calcButton", "Calculate")
         ),
     
-    column(3,
+    column(3, br(), 
        sliderInput("mean", "Mean: ",
                    min = 0, max = 5000, value = 1600),
        sliderInput("std", "Std: ",
@@ -34,7 +30,7 @@ fluidPage(
        sliderInput("ssrt", "SSRT: ",
                    min = 0 , max = 1000, value = 250)
           ),
-    column(3,
+    column(3, br(),
        sliderInput("dif_1", "Delay 1: ",
                    min = -1000, max = 1, value = -800),
        sliderInput("dif_2", "Delay 2: ",
@@ -42,7 +38,7 @@ fluidPage(
        sliderInput("dif_3", "Delay 3: ",
                    min = -1000, max = 1, value = -600)
            ),
-    column(3,
+    column(3, br(),
        sliderInput("dif_4", "Delay 4: ",
                    min = -1000, max = 1, value = -500),
        sliderInput("dif_5", "Delay 5: ",
